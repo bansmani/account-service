b25bff1adef38e4ac7d738ac0f65fe43dfcd0468
manish bansal <m_bansal10@yahoo.com>
2019-09-22 17:43:37 +0530

    simplified lock manager, for given use case

--- lockmanager-service/src/main/kotlin/LockMangerService.kt
+++ lockmanager-service/src/main/kotlin/LockMangerService.kt
@@ -1,65 +1,53 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.annotations.TestOnly
 
 object LockMangerService : IlockManagerService {
 
-    private val lockCollection = HashSet<Lock>()
+    private val lockCollection = HashSet<String>()
 
-    override fun releaseLock(lock: Lock): Boolean {
+    override fun releaseLock(lock: String): Boolean {
         if (!lockCollection.remove(lock)) throw LockReleaseException("Lock already Expired")
         return true
     }
 
-    override fun acquireDebitTransactionLock(lockName: String, timeout: Long): Lock {
-        val lock = Lock(lockName, LockType.DEBIT_ACCOUNT_ENTRY_LOCK)
-        return tryAquireLock(lock, timeout)
-    }
-
-    //Mutually Exclusive keep longer timeout
-    //caller must retry on exception
-    override fun acquireBalanceUpdateLock(lockName: String, timeout: Long): Lock {
-        val lock = Lock(lockName, LockType.BALANCE_UPDATE_LOCK)
-        return tryAquireLock(lock, timeout)
-    }
-
-    private fun tryAquireLock(lock:Lock, timeout: Long): Lock {
+    override fun acquireLock(lock: String, timeout: Long): Boolean {
         var counter = timeout + 1
         while (counter-- > 0) {
             if (lockCollection.add(lock)) {
-                return lock
+                return true
             } else {
                 runBlocking {
                     delay(1)
                 }
             }
         }
-        throw LockAquireFailed(lock.lockName)
+        throw LockAquireFailed(lock)
     }
 
+    //for testing, should not go in production
+    @TestOnly
     private fun clearLockCollection() {
         lockCollection.clear()
     }
 
 }
 
-class LockReleaseException(s: String) : Throwable(s)
+class LockReleaseException(lockName: String) : Throwable(lockName)
 class LockAquireFailed(lockName: String) : Throwable(lockName)
 
 
 interface IlockManagerService {
-    fun acquireDebitTransactionLock(lockName: String, timeout: Long): Lock
-    fun acquireBalanceUpdateLock(lockName: String, timeout: Long = 0): Lock
-    fun releaseLock(lock: Lock): Boolean
+    fun acquireLock(lockName: String, timeout: Long = 0): Boolean
+    fun releaseLock(lockName: String): Boolean
 }
 
-enum class LockType {
-    BALANCE_UPDATE_LOCK,
-    DEBIT_ACCOUNT_ENTRY_LOCK
-}
 
-data class Lock(val lockName: String, val lockType: LockType) {
-    //lockType
-    //makLockDuration
-    //lockCreationTime
-    //lockExpiryTime
-}
+//proposed for TTL lock including automatic expiry
+//data class Lock(val lockName: String) {
+//  lockType (autoexpire lock)
+//  LockDuration
+//  lockCreationTime
+//  lockExpiryTime
+//  LockOwner
+//}
--- lockmanager-service/src/test/kotlin/LockManagerTest.kt
+++ lockmanager-service/src/test/kotlin/LockManagerTest.kt
@@ -7,41 +7,69 @@ class LockManagerTest {
     @Test
     fun `try to acquire balance update lock with its not present`() {
         callPrivate(LockMangerService, "clearLockCollection")
-        val lock = LockMangerService.acquireBalanceUpdateLock("1234")
-        assertEquals("1234", lock.lockName)
+        assertTrue(LockMangerService.acquireLock("1234"), "able to acquire lock")
     }
-
 
     @Test
     fun `try to acquire balance update lock when its already locked`() {
         callPrivate(LockMangerService, "clearLockCollection")
-        LockMangerService.acquireBalanceUpdateLock("1234")
+        LockMangerService.acquireLock("1234")
         assertThrows(LockAquireFailed::class.java) {
-            LockMangerService.acquireBalanceUpdateLock("1234")
+            LockMangerService.acquireLock("1234")
         }
     }
 
     @Test
     fun `try to acquire balance update lock when its already locked with timeout in range`() {
         callPrivate(LockMangerService, "clearLockCollection")
-        LockMangerService.acquireBalanceUpdateLock("1234")
-        var lock: Lock? = null
+        LockMangerService.acquireLock("1234")
+        var isLocked = false
         GlobalScope.launch {
-            lock = LockMangerService.acquireBalanceUpdateLock("1234", 2000)
+           isLocked =  LockMangerService.acquireLock("1234", 2000)
         }
         runBlocking { delay(100) }
-        callPrivate(LockMangerService, "clearLockCollection")
+        LockMangerService.releaseLock("1234")
         runBlocking { delay(2) }
-        assertEquals("1234", lock?.lockName)
+        assertTrue(isLocked)
     }
 
 
     @Test
-    fun `try to acquire balance update lock when its already locked with timeout hit`() {
+    fun `try to acquire balance update lock when its already locked and hit timeout`() {
         callPrivate(LockMangerService, "clearLockCollection")
-        LockMangerService.acquireBalanceUpdateLock("1234")
+        LockMangerService.acquireLock("1234")
         assertThrows(LockAquireFailed::class.java) {
-            LockMangerService.acquireBalanceUpdateLock("1234",100)
+            LockMangerService.acquireLock("1234", 100)
+        }
+    }
+
+    @Test
+    fun `release and aquire lock`() {
+        callPrivate(LockMangerService, "clearLockCollection")
+        LockMangerService.acquireLock("1234")
+        assertThrows(LockAquireFailed::class.java) {
+            LockMangerService.acquireLock("1234")
+        }
+        LockMangerService.releaseLock("1234")
+        LockMangerService.acquireLock("1234")
+    }
+
+    @Test
+    fun `release lock which was never aquired`() {
+        callPrivate(LockMangerService, "clearLockCollection")
+        assertThrows(LockReleaseException::class.java) {
+            LockMangerService.releaseLock("1234")
+        }
+    }
+
+
+    @Test
+    fun `release same lock twice`() {
+        callPrivate(LockMangerService, "clearLockCollection")
+        LockMangerService.acquireLock("1234")
+        LockMangerService.releaseLock("1234")
+        assertThrows(LockReleaseException::class.java) {
+            LockMangerService.releaseLock("1234")
         }
     }
 }
